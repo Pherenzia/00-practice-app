45f6bcf27aacff62643fd9a6f46d78801266b6cc
Mitchell <d.r.mitchellriley@gmail.com>
2021-11-19 11:49:09 -0800

    commit:

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+// Here we destructure our props into their own distinct variables
+export default function Home({ onLogout }) {
+  return (
+    <div>
+      <span role="img" aria-label="tada">
+        🎉
+      </span>
+      <h3>You are signed in!</h3>
+      <button type="button" onClick={onLogout}>
+        Log out
+      </button>
+    </div>
+  );
+}
